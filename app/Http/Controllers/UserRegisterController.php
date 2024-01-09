db683a07cfddb9840286df5233c669daf8a519e0
destafajri <>
2024-01-09 14:28:27 +0700

    Adding logic soft delete for register user

--- app/Http/Controllers/UserRegisterController.php
+++ app/Http/Controllers/UserRegisterController.php
@@ -18,7 +18,7 @@ class UserRegisterController extends Controller
     {
         // checking if user already verified and registered
         $user = DB::select("
-                SELECT 1 FROM users WHERE email = ? AND email_verified_at is not null
+                SELECT 1 FROM users WHERE email = ? AND email_verified_at is not null AND deleted_at is null limit 1
             ", [
             $request->email
         ]);
@@ -26,6 +26,25 @@ class UserRegisterController extends Controller
         if (count($user) == 1) {
             return response()->json([
                 'error' => 'user already exist'
+            ], 400);
+        }
+
+        // checking if users already deleted 2 days
+        $user = DB::select("
+                SELECT 1
+                FROM users
+                WHERE email = ?
+                AND deleted_at IS NOT NULL
+                AND deleted_at >= DATE_SUB(?, INTERVAL 2 DAY)
+                LIMIT 1;
+            ", [
+            $request->email,
+            Carbon::now()
+        ]);
+
+        if (count($user) == 1) {
+            return response()->json([
+                'error' => 'please wait 2 days after request delete'
             ], 400);
         }
 
@@ -37,9 +56,11 @@ class UserRegisterController extends Controller
                 name = VALUES(name),
                 email = VALUES(email),
                 password = VALUES(password),
-                updated_at = VALUES(updated_at);
+                updated_at = VALUES(updated_at),
+                deleted_at = null,
+                email_verified_at = null
             ", [
-            // payload if request is register new user
+            // payload request is register new user
             $request->name,
             $request->email,
             Hash::make($request->password),
@@ -48,7 +69,7 @@ class UserRegisterController extends Controller
         ]);
 
         $data = DB::select('
-                SELECT * FROM users WHERE email = ?
+                SELECT * FROM users WHERE email = ? limit 1
             ', [
             $request->email
         ]);
