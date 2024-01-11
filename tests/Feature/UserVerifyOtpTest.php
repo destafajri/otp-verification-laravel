011445036b7af56a8b94c49e80464784096b18da
destafajri <>
2024-01-11 13:35:17 +0700

    Add feature test

---
+++ tests/Feature/UserVerifyOtpTest.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace Tests\Feature;
+
+use Carbon\Carbon;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Redis;
+use Illuminate\Support\Sleep;
+use Tests\TestCase;
+
+class UserVerifyOtpTest extends TestCase
+{
+    public function testUserInputValidOtp(): void
+    {
+        // setup
+        $email = "test@gmail.com";
+        $otp = "123456";
+        $key = "OTP-email_" . $email;
+        $expTime = 300;
+        DB::insert("INSERT INTO users (name, email, password, email_verified_at, deleted_at)
+        VALUES(?, ?, ?, ?, ?)", [
+            "test",
+            $email,
+            "password",
+            Carbon::now(),
+            Carbon::now()->subDay(1)
+        ]);
+
+        Redis::setex($key, $expTime, $otp);
+
+        // action
+        $response = $this->post("/api/otp/verify", [
+            "email" => "test@gmail.com",
+            "otp" => "123456"
+        ]);
+
+        // assertion
+        $response->assertStatus(201)
+            ->assertJson([
+                'message' => "register success"
+            ]);
+        self::assertNull(
+            Redis::get($key)
+        );
+        self::assertNotNull(
+            Auth::user()
+        );
+    }
+
+    public function testUserInputInvalidOtp(): void
+    {
+        // setup
+        $email = "test@gmail.com";
+        $otp = "123456";
+        $key = "OTP-email_" . $email;
+        $expTime = 300;
+        DB::insert("INSERT INTO users (name, email, password, email_verified_at, deleted_at)
+        VALUES(?, ?, ?, ?, ?)", [
+            "test",
+            $email,
+            "password",
+            Carbon::now(),
+            Carbon::now()->subDay(1)
+        ]);
+
+        Redis::setex($key, $expTime, $otp);
+
+        // action
+        $response = $this->post("/api/otp/verify", [
+            "email" => "test@gmail.com",
+            "otp" => "123457"
+        ]);
+
+        // assertion
+        $response->assertStatus(400)
+            ->assertJson([
+                'message' => "otp doesn't valid"
+            ]);
+        self::assertNotNull(
+            Redis::get($key)
+        );
+        self::assertNull(
+            Auth::user()
+        );
+    }
+
+    public function testUserInputExpiredOtp(): void
+    {
+        // setup
+        $email = "test@gmail.com";
+        $otp = "123456";
+        $key = "OTP-email_" . $email;
+        $expTime = 1;
+        DB::insert("INSERT INTO users (name, email, password, email_verified_at, deleted_at)
+        VALUES(?, ?, ?, ?, ?)", [
+            "test",
+            $email,
+            "password",
+            Carbon::now(),
+            Carbon::now()->subDay(1)
+        ]);
+
+        Redis::setex($key, $expTime, $otp);
+
+        // action
+        Sleep::for(2)->second();
+        $response = $this->post("/api/otp/verify", [
+            "email" => "test@gmail.com",
+            "otp" => "123456"
+        ]);
+
+        // assertion
+        $response->assertStatus(400)
+            ->assertJson([
+                'message' => "otp doesn't valid"
+            ]);
+        self::assertNull(
+            Redis::get($key)
+        );
+        self::assertNull(
+            Auth::user()
+        );
+    }
+}
